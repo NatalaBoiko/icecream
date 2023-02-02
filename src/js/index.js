7ea95047dc6a43fe29bad083331ce125f07d4bf2
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2023-02-02 16:15:48 +0200

    howItsMade adaptive

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+// console.log(parseInt('10%') ** 2);
+// const sal = 1000;
+// const tax = '10%';
+// console.log(tax);
+// console.log(parseInt(tax));
+// const taxNum = parseInt(tax) / 100;
+// const taxSal = sal + sal * taxNum + sal * taxNum ** 2 + sal * taxNum ** 3;
+// console.log(taxSal);
