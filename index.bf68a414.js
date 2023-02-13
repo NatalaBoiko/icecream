3333f0a798d9f6aedb64564bbab563e16a7cea78
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2023-02-13 17:41:28 +0000

    Deploying to gh-pages from @ NatalaBoiko/icecream@134e166dc842996c5c23396db615d5385f7bed1d 🚀

--- index.bf68a414.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"bullets",clickable:"true"}});
-//# sourceMappingURL=index.bf68a414.js.map
