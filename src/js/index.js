c22ad58e6010a2c906270de53d43c4db79fd8dce
NatalaBoiko <92225403+NatalaBoiko@users.noreply.github.com>
2023-02-13 15:47:47 +0200

    mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,13 @@
 // const taxSal = sal + sal * taxNum + sal * taxNum ** 2 + sal * taxNum ** 3;
 // console.log(taxSal);
 import slider from './slider';
+
+// function disableScroll() {
+//   window.scrollTo(0, 0);
+// }
+//or
+// const disableScroll = () => {
+//   window.scrollTo(0, 0);
+// };
+
+// window.addEventListener('scroll', disableScroll);
